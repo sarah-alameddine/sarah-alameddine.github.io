30d6cba9291488a0cf5d7ff60deb0baa13450d38
sarah <s-alameddine@hotmail.com>
2024-12-12 21:36:18 +1100

    Added basic structure and date functionality

---
+++ docs/main.js
@@ -0,0 +1,18 @@
+//TODO: when done convert to typesscrtp
+const countdownToLom = () => {
+    // alert("hello maaam ");
+    console.log("@ Date.now()",Date.now());
+    
+    const currentDate = new Date();
+    const lombokDate = new Date("Thu Dec 14 2024 20:09:50");
+    console.log(currentDate);
+    console.log(lombokDate);
+    const lomMS = lombokDate.getTime() - currentDate.getTime();
+    const lomDay = Math.ceil(lomMS / (1000 * 60 * 60 * 24));
+    console.log("lomDay",Math.abs(lomDay));
+
+    return lomDay;
+};
+
+// document.getElementById("myButton").addEventListener("click", countdownToLom);
+document.getElementById("displayCountDown").textContent = `Days left : ${countdownToLom()}`
